4a6343a06e97f4d2192907d49d3777411a0a58df
Paulo Fernandes <paulojdf27@gmail.com>
2021-09-10 15:04:18 +0100

    add solved exercises using grep command

---
+++ practice_linux_shell/grep_#3.sh
@@ -0,0 +1,6 @@
+: '
+Problem: 
+You are given a text file that will be piped into your command through STDIN. Use grep to remove all those lines that contain the word 'that'. The search should NOT be sensitive to case. 
+'
+#!/bin/bash
+grep -viw 'that'
