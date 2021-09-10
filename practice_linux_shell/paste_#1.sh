32d446862a72732743902e5edc63aa11a25c358e
Paulo Fernandes <paulojdf27@gmail.com>
2021-09-10 09:15:49 +0100

    add command paste related solved exercises

---
+++ practice_linux_shell/paste_#1.sh
@@ -0,0 +1,7 @@
+: '
+Problem: 
+You are given a CSV file where each row contains the name of a city and its state separated by a comma. Your task is to replace the newlines in the file with semicolons as demonstrated in the sample. 
+'
+
+#!/bin/bash
+paste -sd ";"
